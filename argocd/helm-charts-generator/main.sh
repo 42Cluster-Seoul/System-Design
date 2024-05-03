9533abc8a412cb4e98770ccdcece58766d7ccc36
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-05-03 15:48:04 +0900

    사용자 입력에 따른 values.yaml 파일 생성

--- argocd/helm-charts-generator/main.sh
+++ argocd/helm-charts-generator/main.sh
@@ -1,16 +1,31 @@
 # Variables 
 OUTPUT_DIRECTORY="output"
+OUTPUT_CHARTS_FILE="$OUTPUT_DIRECTORY/Charts.yaml"
+OUTPUT_VALUES_FILE="$OUTPUT_DIRECTORY/values.yaml"
 HELM_TEMPLATES_DIRECTORY="template/templates"
 
 if [ ! -d "$OUTPUT_DIRECTORY" ]; then
   mkdir -p "$OUTPUT_DIRECTORY"
 fi
 
-read -p "Enter the value for application name: " app_name
-sed "s/\${app_name}/$app_name/g" template/Charts.yaml.template > $OUTPUT_DIRECTORY/Charts.yaml
+cp template/values.yaml.template $OUTPUT_VALUES_FILE
+cp template/Charts.yaml.template $OUTPUT_CHARTS_FILE
 
+read -p "Enter the value for application name: " app_name
+sed -i.tmp "s/\${app_name}/$app_name/g" $OUTPUT_CHARTS_FILE
+
+read -p "Enter replicas count: " replicas_count
 read -p "Enter image url: " image_url
-sed "s/\${image_url}/$image_url/g" template/values.yaml.template > $OUTPUT_DIRECTORY/values.yaml
+read -p "Enter port number: " port_number
+
+sed -i.tmp \
+    -e "s/\${app_name}/$app_name/g" \
+    -e "s/\${replicas_count}/$replicas_count/g" \
+    -e "s/\${image_url}/$image_url/g" \
+    -e "s/\${port_number}/$port_number/g" \
+    "$OUTPUT_VALUES_FILE"
+
+rm -f "$OUTPUT_CHARTS_FILE.tmp" "$OUTPUT_VALUES_FILE.tmp"
 
 cp -r "$HELM_TEMPLATES_DIRECTORY" "$OUTPUT_DIRECTORY"
 
